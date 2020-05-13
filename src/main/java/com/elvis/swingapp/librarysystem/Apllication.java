d1cf424dcaf465eb692148d18dce2bebfb47a41f
Kwizera Aime Elvis <kwizeraelvis6@gmail.com>
2020-05-13 13:41:26 +0200

    Refactors(Remove

--- src/main/java/com/elvis/swingapp/librarysystem/Apllication.java
+++ src/main/java/com/elvis/swingapp/librarysystem/Apllication.java
@@ -4,7 +4,7 @@ import com.elvis.swingapp.librarysystem.view.MainView;
 
 public class Apllication {
    
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException {
         MainView mainView = new MainView();
         mainView.setVisible(true);
     }
